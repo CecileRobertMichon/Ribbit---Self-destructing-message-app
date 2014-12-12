a1efaf2810cb70679d8205120089ac5a382ae356
Cecile Robert-Michon <cecile.robert-michon@mail.mcgill.ca>
2014-12-11 21:38:59 -0500

    Initial Commit

---
+++ src/com/cecilerm/ribbit/InboxFragment.java
@@ -0,0 +1,22 @@
+package com.cecilerm.ribbit;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class InboxFragment extends Fragment{
+
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.fragment_inbox, container,
+				false);
+		return rootView;
+	}
+	
+}
---
+++ src/com/cecilerm/ribbit/ParseConstants.java
@@ -0,0 +1,7 @@
+package com.cecilerm.ribbit;
+
+public final class ParseConstants {
+
+	public static final String KEY_USERNAME = "username";
+	public static final String KEY_FRIENDS_RELATION = "friendsRelation";
+}
